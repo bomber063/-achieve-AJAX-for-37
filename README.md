5977d6951fbb47bbe8d4f701ae81366c6fa40acc
bomber <tangyihong063@163.com>
2019-06-24 20:07:28 +0800

    介绍如何设置HTTP请求里面第二部分请求头代码及说明

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # -achieve-AJAX-for-37
+## HTTP各部分与代码的关系
+### HTTP请求行的内容
+* 请求行包括请求：
+1. 方法(例如get,post等)
+2. 请求对应的路径，它和报文头的Host属性组成完成的请求URL
+3. 协议名称和版本号
+* 我们可以用代码配置
+* 有路径，协议和Host
+```
+request.open('get','http://bomber.com:8001/xxx')
+```
+或者只有路径
+```
+request.open('get','/xxx')
+```
+
+### 请求头的内容
+* 请求头有Host，这个Host前面一部分bomber.com:8001就是啦
+```
+request.open('get','http://bomber.com:8001/xxx')
+```
+* 请求头的其他内容，比如content-type可以设置吗
+
+### JS可以设置任意请求头（header）吗
+* 在谷歌上面搜js set header可找到，或者直接找MDN，然后可以找一个API——[XMLHttpRequest.setRequestHeader()](https://developer.mozilla.org/zh-CN/docs/Web/API/XMLHttpRequest/setRequestHeader)是设置HTTP请求头部的方法。此方法必须在open()方法和 send()之间调用。如果多次对同一个请求头赋值，只会生成一个合并了多个值的请求头。
+* 前端部分代码,我们在open和send之间插入：
+```
+  request.setRequestHeader('bomber', '18')//注意这里都是字符串
+```
+* 之后我们就可以在打开开发者工具在NetWork->All->xxx->Request Headers里面点击view source可以看到自己添加的bomber：18
+* 所以HTTP请求里面第二部分请求头是可以自己设置，也可以是随便设置的。
+
+### 第三部分是一个回车所以不需要设置
---
+++ main.js
@@ -0,0 +1,133 @@
+// window.jQuery = function(nodeOrSelector){
+//   let nodes = {}
+//   nodes.addClass = function(){}
+//   nodes.html = function(){}
+//   return nodes
+// }
+// window.$ = window.jQuery
+
+// window.Promise = function(fn){
+//   // ...
+//   return {
+//     then: function(){}
+//   }
+// }
+
+// window.jQuery.ajax = function({url, method, body, headers}){
+//   return new Promise(function(resolve, reject){
+//     let request = new XMLHttpRequest()
+//     request.open(method, url) // 配置request
+//     for(let key in headers) {
+//       let value = headers[key]
+//       request.setRequestHeader(key, value)
+//     }
+//     request.onreadystatechange = ()=>{
+//       if(request.readyState === 4){
+//         if(request.status >= 200 && request.status < 300){
+//           resolve.call(undefined, request.responseText)
+//         }else if(request.status >= 400){
+//           reject.call(undefined, request)
+//         }
+//       }
+//     }
+//     request.send(body)
+//   })
+// }
+
+// myButton.addEventListener('click', (e)=>{
+//   let promise = window.jQuery.ajax({
+//     url: '/xxx',
+//     method: 'get',
+//     headers: {
+//       'content-type':'application/x-www-form-urlencoded',
+//       'frank': '18'
+//     }
+//   })
+
+//   promise.then(
+//     (text)=>{console.log(text)},
+//     (request)=>{console.log(request)}
+//   )
+
+// })
+
+// myButton.addEventListener('click',()=>{
+//   let request=new XMLHttpRequest()
+//   request.open('GET','/xxx')
+//   request.send()
+//   setInterval(() => {
+//     console.log(request.readyState)
+//   }, 0);
+// })
+
+myButton.addEventListener('click', function (e) {
+  let request = new XMLHttpRequest()
+  request.open('get', '/xxx')
+  request.setRequestHeader('bomber', '18')
+  request.send()
+
+  request.onreadystatechange = function () {
+    if (request.readyState === 4) {
+      console.log('说明请求完毕')
+      console.log(request.status)
+      if (request.status >= 200 && request.status < 300) {
+        let string = request.responseText
+        let object = window.JSON.parse(string)//把符合JSON语法的字符串转换成JS对应的值（这里就是对象）。这个值包括数组，函数，布尔等等
+
+      }
+      else if (request.status >= 400) {
+        console.log('说明请求失败')
+      }
+    }
+  }
+
+})
+
+
+// myButton.addEventListener('click', function (e) {
+//   $.ajax({
+//     url:'/xxx',
+//     method:'get',
+//   }).then(
+//     (responseText)=>{
+//       console.log(responseText);
+//         return '第一次的结果'
+//     }, 
+//     (request)=>{
+//       console.log('error1')
+//       return '已经处理'
+//     }).then(
+//       (r)=>{
+//         console.log(r+'第二次的结果')
+//       },
+//       (a)=>{console.log('a'+a)}
+//     )
+// })
+
+// myButton.addEventListener('click', function (e) {
+//   $.ajax({
+//     url:'/xxx',
+//     method:'get',
+//   }).then(
+//     (responseText)=>{
+//       console.log(responseText);
+//         return responseText
+//     },
+//     (request)=>{
+//       console.log('error1')
+//       return '已经处理'
+//     }).then(
+//       (上一次的处理结果)=>{
+//         console.log(上一次的处理结果)
+//       },
+//       (request)=>{console.log('error2')}
+//     )
+// })
+
+
+
+
+
+
+
+
